7f972677574dc7ed5f0fd498fbc9e21f5bf5d3c8
mkpfd <2023s19990@stu.cmb.ac.lk>
2026-07-27 15:46:26 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/roboticsclub/RoboticsClubApplication.java
@@ -0,0 +1,12 @@
+package com.roboticsclub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoboticsClubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoboticsClubApplication.class, args);
+    }
+}
